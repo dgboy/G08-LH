67eba8e21e928dc3e361d087faedac7ff07d2b85
dgboy <dan.gorbunov.00@yandex.ru>
2020-07-21 03:23:55 +0300

    Day 21 Mades: 4 Lvl 63

---
+++ Assets/Scripts/Enemies/RockProjectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockProjectile : Projectile {
+    void Start() {
+        
+    }
+
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Objects/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    [Header("Movement")]
+    public float moveSpeed;
+    public Vector2 directionToMove;
+    [Header("Lifetime")]
+    public float lifetime;
+    private float lifetimeSeconds;
+    public Rigidbody2D myRigidbody;
+
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        lifetimeSeconds = lifetime;    
+    }
+
+    void Update() {
+        lifetimeSeconds -= Time.deltaTime;
+        if (lifetimeSeconds <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Launch(Vector2 initialVel) {
+        myRigidbody.velocity = initialVel * moveSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Destroy(this.gameObject);
+    }
+}
