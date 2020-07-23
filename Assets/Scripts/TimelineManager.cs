eed1e753cca5dc3d1ba51870d8a1ad4100ea3e20
dgboy <dan.gorbunov.00@yandex.ru>
2020-07-23 22:02:52 +0300

    part 59 - using timeline for cutscenes (made #70)

---
+++ Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour {
+    public Animator playerAnimator;
+    public RuntimeAnimatorController playerAnim;
+    public PlayableDirector director;
+
+    private bool fix = false;
+
+
+    void Start() {
+        playerAnim = playerAnimator.runtimeAnimatorController;
+        playerAnimator.runtimeAnimatorController = null;
+    }
+
+    void Update() {
+        if (director.state != PlayState.Playing && !fix) {
+            fix = true;
+            playerAnimator.runtimeAnimatorController = playerAnim;
+        }
+    }
+}
