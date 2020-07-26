550eb74fe08d418a0d6bab41914d282f185cdb0b
dgboy <dan.gorbunov.00@yandex.ru>
2020-07-26 03:32:38 +0300

    69 - inventory: scrolling UI (made #75)

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item", order = 0)]
+public class InventoryItem : ScriptableObject {
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemImage;
+    public int numberHeld;
+    public bool usable;
+    public bool unique;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+    
+}
