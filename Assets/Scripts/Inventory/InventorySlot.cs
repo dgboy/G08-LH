0bc4d3a1cc77d643f7df3f1527bf716f31f0c657
dgboy <dan.gorbunov.00@yandex.ru>
2020-07-26 14:16:07 +0300

    70 - inventory: displaying items (made #77)

---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySlot : MonoBehaviour {
+    [Header("Variables from the item")]
+    // public Sprite itemSprite;
+    // public int numberHeld;
+    // public string itemDescription;
+    public InventoryItem thisItem;
+    public InventoryManager thisManager;
+
+    [Header("UI Stuff to change")]
+    [SerializeField] private TextMeshProUGUI itemNumberText;
+    [SerializeField] private Image itemImage;
+
+    public void Setup(InventoryItem newItem, InventoryManager newManager) {
+        thisItem = newItem;
+        thisManager = newManager;
+
+        if (thisItem) {
+            itemImage.sprite = thisItem.itemImage;
+            itemNumberText.text = "" + thisItem.numberHeld;
+        }
+    } 
+
+    void Start() {
+        
+    }
+}
---
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Inventory", menuName = "Inventory/Player Inventory", order = 0)]
+public class PlayerInventory : ScriptableObject {
+    public List<InventoryItem> myInventory = new List<InventoryItem>();
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
